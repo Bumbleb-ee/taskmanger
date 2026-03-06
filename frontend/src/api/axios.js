2b216d5254d3651c628bc0dca05201bbeed3fb20
Bumbleb-ee <nikhilnambiar62@gmail.com>
2026-03-06 23:00:11 +0530

    fix: add /api to the end of the fallback url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: 'https://taskmanger-dv14.onrender.com',
+  baseURL: import.meta.env.VITE_API_URL || 'https://taskmanger-dv14.onrender.com/api',
   withCredentials: true,
 });
 
